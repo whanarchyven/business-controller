d3d2a361afe2e7059ef18c9e9daf73cf8e749bc6
Ivan Zaburdaev <ivanlom227@gmail.com>
2023-08-24 17:36:38 +0300

    remove login btn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -155,7 +155,7 @@
 
                         @if (Route::has('register'))
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                <a class="nav-link" href="{{ route('register') }}"></a>
                             </li>
                         @endif
                     @else
