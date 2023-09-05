5cade178e2308826482949b70beb16cda5f0a64a
Ivan Zaburdaev <ivanlom227@gmail.com>
2023-09-05 06:48:17 +0300

    prepared seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,7 @@ class DatabaseSeeder extends Seeder
 //        $this->call(CitySeeder::class);
 //        $this->call(UserSeeder::class);
 //        $this->call(ManagerCoordinatorSeeder::class);
-//        $this->call(TransactionStateSeeder::class);
-//        $this->call(BudgetSeeder::class);
+        $this->call(TransactionStateSeeder::class);
+        $this->call(BudgetSeeder::class);
     }
 }
